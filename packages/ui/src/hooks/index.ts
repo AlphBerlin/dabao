d80a59577b849c17618544bc06198540909d6c46
Ajith Berlin A <ajithberlin09@gmail.com>
2025-05-10 12:12:08 +0800

    feat: Update TypeScript configuration and component imports, add exports for UI components and hooks

---
+++ packages/ui/src/hooks/index.ts
@@ -0,0 +1,4 @@
+// Export all hooks
+export * from './use-media-query.js';
+export * from './use-mobile.js';
+export * from './use-toast.js';
---
+++ packages/ui/src/index.ts
@@ -0,0 +1,4 @@
+// Export everything from the UI package in a maintainable way
+export * from './lib/index.js';
+export * from './components/index.js';
+export * from './hooks/index.js';
